8bc764ceaddc433dce7aadc4e26268220a8f8579
cmiles74 <twitch@nervestaple.com>
2017-02-04 23:39:07 +0000

    Worked on setting up rooms

--- src/cmiles74/mud/server/mud.clj
+++ src/cmiles74/mud/server/mud.clj
@@ -38,27 +38,54 @@
 ;; map of anonymous client names to their websocket streams
 (def anonymous-clients (ref {}))
 
+;; map of rooms to clients
+(def rooms-clients (ref {}))
+
 ;; our event bus for broadcasts
 (def event-bus (bus/event-bus))
 
-(defn repeat-bot [stream-in stream-out]
+(defn repeat-bot [name stream-in stream-out]
   (stream/consume
    (fn [message]
-     (stream/put! (str "bot: " message) stream-out))
+     (stream/put! (str name " Bot: " message) stream-out))
    stream-in))
+
+(def rooms
+  {1 {:name "Room 1"
+      :description "A red room with a large number \"1\" on the floor."
+      :exits {"east" 2}}
+   2 {:name "Room 2"
+      :description "A green room with a large number \"2\" on the floor."
+      :exits {"west" 1}}})
 
 (defn register-client
   "Registers a new client with the server by assigning them a name and adding
-  their name and websocket stream to the anonymous-clients map."
+  their name and websocket stream to the anonymous-clients map. Returns a map of
+  client data."
   [stream]
-  (dosync (let [client-name (count @anonymous-clients)]
-            (alter anonymous-clients assoc (count @anonymous-clients) stream)
-            client-name)))
+  (dosync (let [client-name (count @anonymous-clients)
+                client-map {:websocket stream
+                            :name (str "Anonymous #" client-name)
+                            :room 1}]
+            (alter anonymous-clients assoc client-name client-map)
+            client-map)))
 
 (defn welcome
   "Writes a welcome message for the provided client-name to the provided websocket stream."
-  [client-name stream]
-  (stream/put! stream (str "Welcome to the Mud Server, Client #" client-name "!")))
+  [client]
+  (stream/put! (:websocket client) (str "Welcome to the Mud Server, Client #" (:name client) "!"))
+  (stream/put! (:websocket client) (str "You open the door and enter a room...")))
+
+(defn describe-room
+  [client]
+  (info ((rooms (:room client)) :description))
+  (stream/put! (:websocket client) ((rooms (:room client)) :description)))
+
+(defn client-handler
+  "Returns a function that will handle incoming and outgoing messages to the client."
+  [client]
+  (fn [message]
+    (bus/publish! event-bus ::broadcast (str (:name client) ": " message))))
 
 (defn websocket-handler
   "Handles an incoming client web request by creating a websocket stream for the
@@ -70,10 +97,17 @@
    (deferred/let-flow [stream (http/websocket-connection request)]
 
      ;; register the new client
-     (let [client-name (register-client stream)]
+     (let [client (register-client stream)
+           handler-fn (client-handler client)]
+
+       ;; place the client in the starting room
+       (dosync (alter rooms-clients assoc (:room client) (:name client)))
 
        ;; welcome the client
-       (welcome client-name stream)
+       (welcome client)
+       (welcome client)
+       ;; describe the room to the client
+       (describe-room client)
 
        ;; subscribe the new stream to our broadcast topic
        (stream/connect
@@ -81,10 +115,8 @@
         stream
         {:timeout 1e4})
 
-       ;; consume all messages and post to broadcast stream
-       (stream/consume
-        (fn [message]
-          (bus/publish! event-bus ::broadcast (str client-name ": " message)))
+       ;; consume and handle all client messages
+       (stream/consume handler-fn
         (stream/throttle
          client-max-message-rate
          client-max-message-backlog
